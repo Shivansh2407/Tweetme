467421851778d69b1b98cc9cb6638d3ca676da1e
Shivansh2407 <shivanshsrivastava2000@gmail.com>
2019-12-11 12:07:13 +0530

    Initial commit

--- tweetme/wsgi.py
+++ tweetme/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tweetme.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tweetme.settings.local")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
